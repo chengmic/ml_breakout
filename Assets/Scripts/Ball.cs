d34f0172ae3cec36fea77e261e4d2dc6914820f7
Shawnpal Kahlon <kahlons@oregonstate.edu>
2023-10-26 21:29:10 -0400

    Ball Spawns on paddle and launches with spacebar

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -10,12 +10,18 @@ public class Ball : MonoBehaviour
     [SerializeField] private Transform paddle;
     public float ball_speed = 6.7f;
 
+    private bool ball_in_play = false;
+
     // Start is called before the first frame update
     void Start()
     {
         // start ball in downard direction
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = Vector2.down * ball_speed;
+        rb.velocity = Vector2.zero;
+        transform.position = paddle.position + new Vector3(0, 0.5f, 0);
+
+
+
     }
 
     // Update is called once per frame
@@ -23,6 +29,17 @@ public class Ball : MonoBehaviour
     {
         if (game_manager.bricks_remaining == 0) {
             this.gameObject.SetActive(false);
+        }
+
+        if (ball_in_play ==  false){
+            transform.position = paddle.position + new Vector3(0, 0.3f, 0);
+            
+            if (Input.GetButtonDown("Jump")){
+                rb.velocity = Vector2.up * ball_speed;
+                ball_in_play = true;
+
+            }
+
         }
     }
 
@@ -41,7 +58,7 @@ public class Ball : MonoBehaviour
                 return;
             }
             // moves ball position above paddle when a new ball spawns
-            transform.position = paddle.position + new Vector3(0, 0.5f, 0);
+            ball_in_play = false;
         }
     }
 }
