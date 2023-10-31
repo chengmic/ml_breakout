2b7ffbfec39eb8e77ee123c9f42f45289118fd44
Michelle Cheng <chengmic@oregonstate.edu>
2023-10-31 16:19:20 -0700

    add navigation to versus mode on main menu; change Level 1V to Level V1; change MainMenu.cs to NavigationManager.cs;

--- Assets/Scripts/NavigationManager.cs
+++ Assets/Scripts/NavigationManager.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class NavigationManager : MonoBehaviour
 {
     public void SinglePlayerStart()
     {
         SceneManager.LoadScene("Level 1");
+    }
+
+    public void VersusModeStart()
+    {
+        SceneManager.LoadScene("Level V1");
     }
 
     public void GoToMainMenu()
