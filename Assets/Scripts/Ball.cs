5b2316d24208bf0202f951c84990ecb68d34b375
coalexa <coalexa98@gmail.com>
2023-11-02 17:30:31 -0700

    code cleanup

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,13 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 
 public class Ball : MonoBehaviour
 {
     Rigidbody2D rb;
     public GameManager game_manager;
-    public MovePaddleAgent paddleAgent;
+    public MovePaddleAgent paddle_agent;
     [SerializeField] private Transform paddle;
     public float ball_speed = 6.7f;
     public bool ball_in_play = false;
@@ -24,29 +21,32 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (ball_in_play ==  false){
+        if (!ball_in_play)
+        {
             // If ball is not in play, reset its position to ontop of the paddle
             transform.position = paddle.position + new Vector3(0, 0.3f, 0);
 
             if (Input.GetButtonDown("Jump"))
             {
                 // When user presses spacebar, send ball upwards from paddle
-                rb.velocity = Vector2.up * ball_speed;
-                ball_in_play = true;
+                Launch();
             }
         }
-        else{
+        else
+        {
             // If ball is in play, check vertical speed. If ball is in motion, provide small reward
             float ball_vertical_velocity = rb.velocity.y;
-            if(ball_vertical_velocity != 0f){
-                paddleAgent.ballStillMoving();
-                }
-                }
+            if (ball_vertical_velocity != 0f)
+            {
+                paddle_agent.BallStillMoving();
+            }
+        }
     }
 
     public void Launch()
     {
         rb.velocity = Vector2.up * ball_speed;
+        ball_in_play = true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -55,12 +55,11 @@ public class Ball : MonoBehaviour
         if (collision.gameObject.CompareTag("Lower Bound"))
         {
             game_manager.ChangeLives(-1);
-            paddleAgent.ballBelowPaddle();
+            paddle_agent.BallBelowPaddle();
 
             if (game_manager.lives_val <= 0)
             {
                 rb.velocity = Vector2.zero;
-                
                 return;
             }
             // Sets in play to false with resets the ball on top of the paddle
