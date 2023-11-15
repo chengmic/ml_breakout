8ab47c12dec7db4a22554a23262f18d2c010b61c
Michelle Cheng <chengmic@oregonstate.edu>
2023-11-15 02:30:58 -0800

    copy game objects from Demo Scene and replace CPU objects in Versus Mode with copied objects in order to maintain local positioning; add observation inputs in obs_0 tensor in CPUPaddle.cs

--- Assets/Scripts/CPUPaddle.cs
+++ Assets/Scripts/CPUPaddle.cs
@@ -21,11 +21,39 @@ public class CPUPaddle : MonoBehaviour
 
     void Update()
     {
+        // Current Ball and Paddle locations
+        Vector2 current_paddle_location = transform.localPosition;
+        Vector2 current_ball_Location = ball.transform.localPosition;
+
+        //Distance from paddle to ball
+        float distance_to_ball = Vector2.Distance(current_paddle_location, current_ball_Location);
+
+        //Direction of ball from paddle
+        Vector2 ball_direction_from_paddle_plus_distance = current_ball_Location - current_paddle_location;
+        Vector2 ball_direction_from_paddle = ball_direction_from_paddle_plus_distance.normalized;
+
+        //Distance From Wall to Paddle
+        float paddle_distance_from_right_wall = current_paddle_location.x - right_x_bound;
+        float paddle_distance_from_left_wall = left_x_bound - current_paddle_location.x;
+
+
+        // inputs
         var inputs = new Dictionary<string, Tensor>();
         inputs["obs_0"] = new Tensor(1, 1, 1, 10);
+        inputs["obs_0"][0] = current_paddle_location.x;
+        inputs["obs_0"][1] = current_paddle_location.y;
+        inputs["obs_0"][2] = current_ball_Location.x;
+        inputs["obs_0"][3] = current_ball_Location.y;
+        inputs["obs_0"][4] = ball.ball_in_play ? 1.0f : 0.0f;
+        inputs["obs_0"][5] = distance_to_ball;
+        inputs["obs_0"][6] = ball_direction_from_paddle.x;
+        inputs["obs_0"][7] = ball_direction_from_paddle.y;
+        inputs["obs_0"][8] = paddle_distance_from_right_wall;
+        inputs["obs_0"][9] = paddle_distance_from_left_wall;
         inputs["action_masks"] = new Tensor(1, 1, 1, 2);
         worker.Execute(inputs);
 
+        // outputs
         Tensor discrete_actions = worker.PeekOutput("discrete_actions");
         Tensor continuous_actions = worker.PeekOutput("continuous_actions");
 
@@ -33,6 +61,7 @@ public class CPUPaddle : MonoBehaviour
         // Debug.Log(discrete_actions[0]);
         // Debug.Log(continuous_actions[0]);
 
+        // dispose
         inputs["obs_0"].Dispose();
         inputs["action_masks"].Dispose();
 
@@ -46,9 +75,9 @@ public class CPUPaddle : MonoBehaviour
         float move_x = continuous_actions[0];
         float speed = 10f;
 
-        Vector3 new_position = transform.position + new Vector3(move_x, 0f) * Time.deltaTime * speed;
+        Vector3 new_position = transform.localPosition + new Vector3(move_x, 0f) * Time.deltaTime * speed;
         new_position.x = Mathf.Clamp(new_position.x, left_x_bound, right_x_bound);
-        transform.position = new_position;
+        transform.localPosition = new_position;
 
         // use this to track the paddle position
         // Debug.Log($"new_position.x: {new_position.x}");
