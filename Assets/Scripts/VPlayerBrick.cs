62647447a972fc0e6a6641cf20b166f3c33f76d4
Michelle Cheng <chengmic@oregonstate.edu>
2023-11-20 14:36:57 -0800

    remove powerups for Level V1 and Level V2

---
+++ Assets/Scripts/VPlayerBrick.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class VPlayerBrick : MonoBehaviour
+{
+    public PlayerGameManager player_game_manager;
+
+    void Start()
+    {
+        if (player_game_manager == null)
+        {
+            player_game_manager = FindObjectOfType<PlayerGameManager>();
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // destory brick
+        if (collision.gameObject.CompareTag("PlayerBall"))
+        {
+            gameObject.SetActive(false);
+            player_game_manager.ChangeScore(100);
+            player_game_manager.ReduceBrickCount();
+        }
+    }
+}
